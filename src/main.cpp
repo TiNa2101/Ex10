f7fcadbe757e49bab305f5399e475b0a2d7dbdc2
TiNa2101 <71177685+TiNa2101@users.noreply.github.com>
2021-01-19 18:15:00 +0300

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -3,8 +3,7 @@
 #include <iostream>
 #include <string>
 
-int main()
-{
+int main() {
     std::string s1("(6 * (4 - 2) + 5) * (2.6 + 3 * 7) - 9");
     std::string s2 = infix2prefix(s1);
     std::cout << s2;  // 6 4 2 - * 5 + 2.6 3 7 * + * 9 -
