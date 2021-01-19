1c38f6a560a8ea0bb6550c6a8462e81fb84eba06
TiNa2101 <71177685+TiNa2101@users.noreply.github.com>
2021-01-19 19:13:05 +0300

    Update postfix.h

--- include/postfix.h
+++ include/postfix.h
@@ -1,8 +1,8 @@
 // Copyright 2021 Ex10 TiNa
 #ifndef INCLUDE_POSTFIX_H_
 #define INCLUDE_POSTFIX_H_
+#include <string>
 #include <iostream>
-#include <string.h>
 
 std::string infix2prefix(std::string inf);
 int Priority(char oper);
